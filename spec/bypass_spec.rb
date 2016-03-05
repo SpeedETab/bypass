c31e3a18ef4c9a9b093d9439a4ba0ff970f97348
Damon Davison <damon@curatur.com>
2016-03-05 15:03:12 -0500

    Add dummy spec file

---
+++ spec/bypass_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Bypass do
+  it 'has a version number' do
+    expect(Bypass::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
