710721969cfa31eb7202857e0815e9a78111de73
Joseworks <josefernandez@joseworks.org>
2016-10-24 12:37:28 -0400

    Implement prevision for hash object

--- lib/bypass/menu.rb
+++ lib/bypass/menu.rb
@@ -7,7 +7,18 @@ module Bypass
     def initialize(opts = {})
       @concession_id = opts.fetch(:concession_id)
       @meta = opts[:menu]["meta"]
-      parsed_menu = JSON.parse opts[:menu]
+
+      begin
+        if opts[:menu].respond_to?(:json)
+          parsed_menu = JSON.parse opts[:menu]
+        elsif opts[:menu].respond_to?(:hash)
+          parsed_menu = opts[:menu]
+        end
+
+      rescue ArgumentError => e
+        Rails.logger.error(" The error raised was #{e.inspect}")
+      end
+
       @categories = CategoryCollection.new parsed_menu["categories"]
     end
 
